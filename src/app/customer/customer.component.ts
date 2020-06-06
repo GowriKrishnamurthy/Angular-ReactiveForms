7a9f5f0d5d7737c92f6bb63ce6c0ba62a8013463
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-06-06 21:49:35 +1000

    adjusting validation rules at runtime

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Customer } from './customer';
 
 
@@ -16,9 +16,11 @@ export class CustomerComponent implements OnInit {
   ngOnInit() {
 
     this.customerForm = this.formBuilder.group({
-      firstName: ['',Validators.required,Validators.minLength(3)],
-      lastName: ['',Validators.required,Validators.minLength(50)],
-      email: ['',Validators.required,Validators.email],
+      firstName: ['',[Validators.required,Validators.minLength(3)]],
+      lastName: ['',[Validators.required,Validators.maxLength(50)]],
+      email: ['', [Validators.required,Validators.email]],
+      phone:'',
+      notification:'email',
       sendCatalog: true      
     });
 
@@ -26,6 +28,8 @@ export class CustomerComponent implements OnInit {
     //   firstName: new FormControl(),
     //   lastName: new FormControl(),
     //   email: new FormControl(),
+    //   phone: new FormControl(),
+    //   notification: new FormControl('email'),
     //   sendCatalog: new FormControl(true),
     // });
   }
@@ -41,5 +45,16 @@ export class CustomerComponent implements OnInit {
       sendCatalog: false
     });
   }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
+  }
+
 
 }
