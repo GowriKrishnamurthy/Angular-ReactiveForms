0e5b16500d92462555e7d84d9128a71430ac3da5
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-06-07 22:38:00 +1000

    Custom validators with parameters

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -2,6 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl, AbstractControl, ValidatorFn } from '@angular/forms';
 import { Customer } from './customer';
 
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+  const emailControl = c.get('email');
+  const confirmControl = c.get('confirmEmail');
+
+  if (emailControl.pristine || confirmControl.pristine) {
+    return null;
+  }
+
+  if (emailControl.value === confirmControl.value) {
+    return null;
+  }
+  return { match: true };
+}
+
 function ratingRange(min: number, max: number): ValidatorFn {
   return (c: AbstractControl): { [key: string]: boolean } | null => {
     if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)) {
@@ -26,7 +40,12 @@ export class CustomerComponent implements OnInit {
     this.customerForm = this.formBuilder.group({
       firstName: ['',[Validators.required,Validators.minLength(3)]],
       lastName: ['',[Validators.required,Validators.maxLength(50)]],
-      email: ['', [Validators.required,Validators.email]],
+      emailGroup: this.formBuilder.group(
+        {
+          email: ['', [Validators.required,Validators.email]],
+          confirmEmail: ['', [Validators.required]],    
+        }, {validator: emailMatcher}
+      ),
       phone:'',
       notification:'email',
       rating: [null, ratingRange(1, 5)],
